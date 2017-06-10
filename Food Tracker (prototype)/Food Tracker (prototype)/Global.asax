5c3fd0405ef52cec034a4c0c6affaa600b35ba21
kitsune144 <p.lebron144@hotmail.com>
2017-06-10 09:36:04 -0400

    Creation
    First upload of the project

---
+++ Food Tracker (prototype)/Food Tracker (prototype)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Food_Tracker__prototype_.MvcApplication" Language="C#" %>
---
+++ Food Tracker (prototype)/Food Tracker (prototype)/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Food_Tracker__prototype_.Startup))]
+namespace Food_Tracker__prototype_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
